6cd92b385877d1a9bc2f5acc064bba4210ed3516
David Venté <davidventepolo@gmail.com>
2023-03-04 16:27:34 -0500

    04-Mar 01 ~ Inicio de actividad 10 sobre app con Rest

---
+++ src/index.js
@@ -0,0 +1,106 @@
+const express = require('express'); //se indica que se requiere express
+const app = express(); // se inicia express y se instancia en una constante de nombre app.
+const morgan = require('morgan'); //se indica que se requiere morgan
+const mysql = require('mysql2/promise');
+// settings
+app.set('port', 3000); //se define el puerto en el cual va a funcionar el servidor
+// Utilities
+app.use(morgan('dev')); //se indica que se va a usar morgan en modo dev
+app.use(express.json()); //se indica que se va a usar la funcionalidad para manejo de json de express
+// Conexión a la base de datos
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'almacen'
+});
+// Rutas para productos
+app.get('/productos', async (req, res) => {
+    const conn = await pool.getConnection();
+    const [rows] = await conn.query('SELECT * FROM productos');
+    conn.release();
+    res.json(rows);
+});
+app.get('/productos/:id', async (req, res) => {
+    const conn = await pool.getConnection();
+    const [rows] = await conn.query('SELECT * FROM productos WHERE id = ?',
+        [req.params.id]);
+    conn.release();
+    if (rows.length === 0) return res.status(404).send('Producto no encontrado');
+    res.json(rows[0]);
+});
+app.post('/productos', async (req, res) => {
+    const conn = await pool.getConnection();
+    const [result] = await conn.query('INSERT INTO productos VALUES (null, ?, ?, ?)', [req.body.nombre, req.body.descripcion, req.body.precio]);
+    const [rows] = await conn.query('SELECT * FROM productos WHERE id = ?',
+        [result.insertId]);
+    conn.release();
+    res.json(rows[0]);
+});
+app.put('/productos/:id', async (req, res) => {
+    const conn = await pool.getConnection();
+    const [result] = await conn.query('UPDATE productos SET nombre=?, descripcion =?, precio =? WHERE id =? ', [req.body.nombre, req.body.descripcion, req.body.precio, req.params.id]);
+    const [rows] = await conn.query('SELECT * FROM productos WHERE id = ?',
+        [req.params.id]);
+    conn.release();
+    res.json(rows[0]);
+});
+app.delete('/productos/:id', async (req, res) => {
+    const conn = await pool.getConnection();
+    const [rows] = await conn.query('DELETE FROM productos WHERE id = ?',
+        [req.params.id]);
+    conn.release();
+    res.send("producto borrado");
+});
+
+//! Rutas para user
+app.get('/user', async (req, res) => {
+    const conn = await pool.getConnection();
+    const [rows] = await conn.query('SELECT * FROM user');
+    conn.release();
+    res.json(rows);
+});
+app.get('/user/:id', async (req, res) => {
+    const conn = await pool.getConnection();
+    const [rows] = await conn.query('SELECT * FROM user WHERE id = ?',
+        [req.params.id]);
+    conn.release();
+    if (rows.length === 0) return res.status(404).send('Usuario no encontrado');
+    res.json(rows[0]);
+});
+app.get('/user/:usuario/:password', async (req, res) => {
+    const conn = await pool.getConnection();
+    const [rows] = await conn.query('SELECT * FROM user WHERE usuario = ? AND password = ?',
+        [req.params.usuario, req.params.password]);
+    conn.release();
+    if (rows.length === 0) return res.status(404).send('Usuario no encontrado');
+    res.json(rows[0]);
+});
+app.post('/user', async (req, res) => {
+    const conn = await pool.getConnection();
+    console.log(req.body)
+    const [result] = await conn.query('INSERT INTO user VALUES (null, ?, ?, ?, ?, ?)', [req.body.nombre, req.body.apellido, req.body.usuario, req.body.password, req.body.telefono]);
+    const [rows] = await conn.query('SELECT * FROM user WHERE id = ?',
+        [result.insertId]);
+    conn.release();
+    res.json(rows[0]);
+});
+app.put('/user/:id', async (req, res) => {
+    const conn = await pool.getConnection();
+    const [result] = await conn.query('UPDATE user SET nombre=?, apellido =?, usuario =?, password =?, telefono =? WHERE id =? ', [req.body.nombre, req.body.apellido, req.body.usuario, req.body.password, req.body.telefono, req.body.id]);
+    const [rows] = await conn.query('SELECT * FROM user WHERE id = ?',
+        [req.params.id]);
+    conn.release();
+    res.json(rows[0]);
+});
+app.delete('/user/:id', async (req, res) => {
+    const conn = await pool.getConnection();
+    const [rows] = await conn.query('DELETE FROM user WHERE id = ?',
+        [req.params.id]);
+    conn.release();
+    res.send("¡usuario borrado!");
+});
+// Iniciar el servidor
+app.listen(app.get('port'), () => {
+    console.log("Servidor funcionando");
+}); //se inicia el servidor en el puerto definido y se pone un mensaje en la consola
